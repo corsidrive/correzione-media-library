668597b9d0fb1a22dcb51036849781359c395c97
corsidrive <corsidrive@gmail.com>
2020-06-21 16:19:36 +0200

    Configurazione locale

--- Christian Morandi/Config.php
+++ Christian Morandi/Config.php
@@ -1,23 +1,25 @@
 <?php
 
+// class Config {
+//     public const DB_NAME = 'media_library';
+//     public const DB_USER = 'root';
+//     public const DB_PASSWORD = '';
+//     public const DB_HOST = 'localhost';
+//     public const SITE_URL = 'http://localhost/tss2020php-morandi/';
+//     public const VIEW_FOLDER = __DIR__.'/view/';
+//     public const UPLOAD_FOLDER = __DIR__.'/uploads/';
+
+// }
+
+
 class Config {
     public const DB_NAME = 'media_library';
     public const DB_USER = 'root';
-    public const DB_PASSWORD = '';
+    public const DB_PASSWORD = 'fiacca';
     public const DB_HOST = 'localhost';
-    public const SITE_URL = 'http://localhost/tss2020php-morandi/';
-    public const VIEW_FOLDER = __DIR__.'/view/';
-    public const UPLOAD_FOLDER = __DIR__.'/uploads/';
-
-}
-
-
-//     public const DB_NAME = 'my_christianmorandi';
-//     public const DB_USER = 'christianmorandi';
-//     public const DB_PASSWORD = '';
-//     public const DB_HOST = 'localhost';
     
-//     public const SITE_URL = 'christianmorandi.altervista.org';
-//     public const VIEW_FOLDER = __DIR__.'/view/';
-//     public const UPLOAD_FOLDER = __DIR__.'/uploads/';
-// }
+    public const SITE_URL = 'http://tss1920-media-library.test/Christian%20Morandi/';
+    public const VIEW_FOLDER = __DIR__.'/view/';
+
+    public const UPLOAD_FOLDER = __DIR__.'/uploads/';
+}
